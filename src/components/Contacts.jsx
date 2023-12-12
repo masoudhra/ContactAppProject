eefc639356b4a9da0f4fbf4527c33d72638c054b
Masoud Ramezani <masoudhra@gmail.com>
2023-12-13 11:21:37 +1300

    make alert for validation

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -3,6 +3,7 @@ import ContactsList from "./ContactsList";
 
 function Contacts() {
   const [contacts, setContacts] = useState([]);
+  const [alert, setAlert] = useState("");
   const [contact, setContact] = useState({
     name: "",
     lastName: "",
@@ -18,13 +19,23 @@ function Contacts() {
   };
 
   const addHandler = () => {
+    if (
+      !contact.name ||
+      !contact.lastName ||
+      !contact.email ||
+      !contact.phone
+    ) {
+      setAlert("Please enter valid data");
+      return;
+    }
+    setAlert("");
     setContacts((contacts) => [...contacts, contact]);
     setContact({
-        name: "",
-        lastName: "",
-        email: "",
-        phone: "",
-    })
+      name: "",
+      lastName: "",
+      email: "",
+      phone: "",
+    });
   };
   return (
     <div>
@@ -59,7 +70,8 @@ function Contacts() {
         />
         <button onClick={addHandler}>Add Contact</button>
       </div>
-      <ContactsList contacts={contacts}/>
+      <div>{alert && <p>{alert}</p>}</div>
+      <ContactsList contacts={contacts} />
     </div>
   );
 }
