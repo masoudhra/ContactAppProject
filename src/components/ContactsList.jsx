ba2d98b21003bdc24b3debc9030385cc552f1020
Masoud Ramezani <masoudhra@gmail.com>
2023-12-13 15:53:26 +1300

    add icon and li in contact list

--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -2,11 +2,26 @@ function ContactsList({ contacts }) {
   return (
     <div>
       <h3>Contacts List</h3>
-      <ul>
-        {contacts.map((contact) => (
-          <li key={contact.id}>{contact.name}</li>
-        ))}
-      </ul>
+      {contacts.length ? (
+        <ul>
+          {contacts.map((contact) => (
+            <li key={contact.id}>
+              <p>
+                {contact.name} {contact.lastName}
+              </p>
+              <p>
+                <span>📪</span> {contact.email}
+              </p>
+              <p>
+                <span>☎</span> {contact.phone}
+              </p>
+              <button>🚮</button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No Contacts Yet!</p>
+      )}
     </div>
   );
 }
